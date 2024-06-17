f9beabddba415bf1c01a602654ce1bd07d9b6488
Robert Sheiman <sheiman.robert.18@gmail.com>
2024-06-17 02:19:08 -0400

    fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,7 +19,7 @@ const theme = createTheme({
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <BrowserRouter>
+      <BrowserRouter basename="/Demoqueue">
         <Routes>
           <Route path="*" element={ <Navigate to="/Demoqueue/home"/>} />
           <Route path="/Demoqueue/home" element={<LandingPage />} />
