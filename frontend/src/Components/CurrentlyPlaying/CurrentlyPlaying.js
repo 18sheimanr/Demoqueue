65623c629638af6c16068b98a182d3b8275d6667
Robert Sheiman <sheiman.robert.18@gmail.com>
2024-06-25 01:22:48 -0400

    Fix Spotify functionaly so this thing actually works

--- frontend/src/Components/CurrentlyPlaying/CurrentlyPlaying.js
+++ frontend/src/Components/CurrentlyPlaying/CurrentlyPlaying.js
@@ -3,34 +3,34 @@ import "./CurrentlyPlaying.css";
 import ShowQRCodeButton from "./ShowQRCodeButton/ShowQRCodeButton";
 
 function CurrentlyPlaying(props) {
-  // const [timeToLock, setTimeToLock] = useState(30);
-
-  //   useEffect(() => {
-  //     setTimeToLock(30);
-  //   }, []);
-
-  //     setInterval(() => {
-  //       setTimeToLock(timeToLock - 1);
-  //     }, 1000);
+  const progressPercentage = (props.songProgress / props.songDuration) * 100;
 
   return (
-    <div className="currentlyPlaying__container">
-      <div className="currentlyPlaying__albumworkName--container">
-        <img
-          className="currentlyPlaying__albumwork"
-          src={props.albumWorkURL}
-          alt={props.name}
-        />
+    <div className="currentlyPlaying__wrapper">
+      <div className="currentlyPlaying__container">
+        <div className="currentlyPlaying__albumworkName--container">
+          <img
+            className="currentlyPlaying__albumwork"
+            src={props.albumWorkURL}
+            alt={props.name}
+          />
 
-        <div className="currentlyPlaying__songInfo">
-          <h2 className="currentlyPlaying__songInfo--name">{props.name}</h2>
-          <h3 className="currentlyPlaying__songInfo--artist">{props.artist}</h3>
+          <div className="currentlyPlaying__songInfo">
+            <h2 className="currentlyPlaying__songInfo--name">{props.name}</h2>
+            <h3 className="currentlyPlaying__songInfo--artist">{props.artist}</h3>
+          </div>
+        </div>
+
+        <div className="currentlyPlaying__lockTimer">
+          <ShowQRCodeButton eventName={props.eventName}/>
         </div>
       </div>
 
-      <div className="currentlyPlaying__lockTimer">
-        {/* <h4 className="lockTimer">Song Locks in {timeToLock}s</h4> */}
-        <ShowQRCodeButton eventName={props.eventName}/>
+      <div className="currentlyPlaying__progressBar">
+        <div
+          className="currentlyPlaying__progressBar--fill"
+          style={{ width: `${progressPercentage}%` }}
+        ></div>
       </div>
     </div>
   );
